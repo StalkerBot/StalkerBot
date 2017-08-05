7f7be2db7ba1ae4f327439b100204cbed1bf15ec
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-05 21:24:06 +0300

    Initial commit

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const Botly = require("botly");
 const botly = new Botly({
-    accessToken: pageAccessToken, //page access token provided by facebook
-    verifyToken: verificationToken, //needed when using express - the verification token you provided when defining the webhook in facebook
-    webHookPath: yourWebHookPath, //defaults to "/",
+    accessToken: EAATww7C4qdMBAGS5lIQBpQVURCfNlGJoxsIYgjFz2Uwj6k03uFbYdvhO737ZCvdY1ZBBetGLq6nRVrygI0OdKu4mZBfxqj7nReGydiZApxpVQWifhJNuqTqRXQEE1KFEPJMSLxVKKGSTVMKhgvdzqbbfOjQZCCLyXGX31ZBL4WtAZDZD, //page access token provided by facebook
+    verifyToken: nadstories, //needed when using express - the verification token you provided when defining the webhook in facebook
+    webHookPath: https://fierce-forest-75439.herokuapp.com/webhook, //defaults to "/",
     notificationType: Botly.CONST.REGULAR //already the default (optional),
 });
 
