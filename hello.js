bdbde3565d7459d7c7fecd735e8dd80a0e27d004
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-05 21:21:03 +0300

    Initial commit

--- hello.js
+++ hello.js
@@ -1,89 +1,21 @@
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
-
-// Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
+const express = require("express");
+const Botly = require("botly");
+const botly = new Botly({
+    accessToken: pageAccessToken, //page access token provided by facebook
+    verifyToken: verificationToken, //needed when using express - the verification token you provided when defining the webhook in facebook
+    webHookPath: yourWebHookPath, //defaults to "/",
+    notificationType: Botly.CONST.REGULAR //already the default (optional),
 });
 
-// Facebook Webhook
-// Used for verification
-app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === "nadstories") {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
-  } else {
-    console.error("Verification failed. The tokens do not match.");
-    res.sendStatus(403);
-  }
-});
+botly.on("message", (senderId, message, data) => {
+    let text = `echo: ${data.text}`;
 
-app.post("/webhook", function (req, res) {
-  // Make sure this is a page subscription
-  if (req.body.object == "page") {
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        }
-      });
+    botly.sendText({
+      id: senderId,
+      text: text
     });
-
-    res.sendStatus(200);
-  }
 });
 
-function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
-
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "Hello my name is Ruaa, and i am here to help :)";
-      sendMessage(senderId, {text: message});
-    });
-  }
-}
-
-// sends message to user
-function sendMessage(recipientId, message) {
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
-    json: {
-      recipient: {id: recipientId},
-      message: message,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log("Error sending message: " + response.error);
-    }
-  });
-}
+const app = express();
+app.use("/webhook", botly.router());
+app.listen(3000);
