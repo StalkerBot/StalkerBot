7ed130fc560fb969fab013e6bf01d1b32ba2a922
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-06 00:54:52 +0300

    N

--- hello.js
+++ hello.js
@@ -1,164 +1,17 @@
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
-
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
-
-// Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
-});
-
-// Facebook Webhook
-// Used for verification
-
-app.get("/webhook", function (req, res) {
-  
-  if (req.query["hub.verify_token"] === "nadstories") {
 var apiai = require('apiai');
-var app = apiai("4921c64eea744c5a8bed203028c5037e");
-    console.log("Verified webhook");
-    
-    res.status(200).send(req.query["hub.challenge"]);
-    
-  } else {
-    
-    console.error("Verification failed. The tokens do not match.");
-    res.sendStatus(403);
-  }
-  
+
+var app = apiai("<your client access token>");
+
+var request = app.textRequest('<Your text query>', {
+    sessionId: '<unique session id>'
 });
 
-app.post("/webhook", function (req, res) {
-  // Make sure this is a page subscription
-  
-  if (req.body.object == "page") {
-    const apiaiApp = require('apiai')(4921c64eea744c5a8bed203028c5037e);
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-         if (event.postback) {
-                    processPostback(event);
-                } else if (event.message) {
-                    processMessage(event);
-                }
-            });
-        });
-
-        res.sendStatus(200);
-    }
+request.on('response', function(response) {
+    console.log(response);
 });
-function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
 
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "Hello my name is StalkerBot, and i am here to help you with information about people :)";
-      sendMessage(senderId, {text: message});
-    });
-  }
-}
-
-// sends message to user
-function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
-
-  let apiai = apiaiApp.textRequest(text, {
-    sessionId: 'tabby_cat' // use any arbitrary id
-  });
-
-  apiai.on('response', (response) => {
-let aiText = response.result.fulfillment.speech;
-
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: {
-        recipient: {id: sender},
-        message: {text: aiText}
-      }
-    }
-    , (error, response) => {
-      if (error) {
-          console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-      }
-    });
- });
-
-
-  apiai.on('error', (error) => {
+request.on('error', function(error) {
     console.log(error);
-  });
+});
 
-  apiai.end();
-}
-
-function processMessage(event) {
-    if (!event.message.is_echo) {
-        var message = event.message;
-        var senderId = event.sender.id;
-
-        console.log("Received message from senderId: " + senderId);
-        console.log("Message is: " + JSON.stringify(message));
-
-        // You may get a text or attachment but not both
-        if (message.text) {
-            var formattedMsg = message.text.toLowerCase().trim();
-
-            // If we receive a text message, check to see if it matches any special
-            // keywords and send back the corresponding movie detail.
-            // Otherwise search for new movie.
-            switch (formattedMsg) {
-                case "hello":
-                case "hi":
-                case "ciao":
-                case "hey":
-                case "bonjour":
-                case "good morning":
-                    sendMessage(senderId, {text: "Hello Buddy, welcome to StalkerBot, do you want me to stalk for you?"});
-                    break;
-		case "what is your name":
-sendMessage(senderId, {text: "My name is StalkerBot, and i am your at your service :)"});
-break;
-
-case "What can you do?":
-sendMessage(senderId, {text: "I can get people's information for you, right now i am working on email extraction :D"});
-break;
-
-
-                default:
-                    sendMessage(senderId, {text: "Hmmm"});
-            }
-        } else if (message.attachments) {
-            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
-        }
-    }
-}
+request.end();
