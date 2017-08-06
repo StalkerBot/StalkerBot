d3a204220e56d2acc3e199fd458e26ac648e955f
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-06 12:36:20 +0300

    L

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
 
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query['hub.verify_token'] === 'botcube is cool');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'nadstories');
 
     if (hubMode && verifyTokenMatches) {
         res.status(200).send(hubChallenge);
