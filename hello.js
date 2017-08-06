8a1e49d611c02b87825edbe1b1889a6280c3e9d8
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-06 12:33:33 +0300

    L

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 var port = process.env.PORT || 5000
 
-server.listen(port, function() {
+app.listen(port, function() {
     console.log("App is running on port " + port);
 });
 
