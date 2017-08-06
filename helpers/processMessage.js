df614f8dabc3eb6b5bd619804044cba7548df32a
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-06 12:29:58 +0300

    L

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,5 +1,5 @@
 const FACEBOOK_ACCESS_TOKEN = '<EAATww7C4qdMBAGS5lIQBpQVURCfNlGJoxsIYgjFz2Uwj6k03uFbYdvhO737ZCvdY1ZBBetGLq6nRVrygI0OdKu4mZBfxqj7nReGydiZApxpVQWifhJNuqTqRXQEE1KFEPJMSLxVKKGSTVMKhgvdzqbbfOjQZCCLyXGX31ZBL4WtAZDZD>';
-const CAT_IMAGE_URL = 'https://botcube.co/public/blog/apiai-tutorial-bot/hosico_cat.jpg';
+const CAT_IMAGE_URL = 'http://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg';
 const API_AI_TOKEN = '<0ac9a2e05c1649048bc8ea02742bf004>';
 const apiAiClient = require('apiai')(API_AI_TOKEN);
 const request = require('request');
