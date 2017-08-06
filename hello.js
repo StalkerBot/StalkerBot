718b8e71a2821af56b8fbb8dcfdf334bf5f29760
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-06 12:37:29 +0300

    L

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var port = process.env.PORT || 5000
+var port = 5000
 
 app.listen(port, function() {
     console.log("App is running on port " + port);
--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,6 +1,6 @@
 const FACEBOOK_ACCESS_TOKEN = '<EAATww7C4qdMBAGS5lIQBpQVURCfNlGJoxsIYgjFz2Uwj6k03uFbYdvhO737ZCvdY1ZBBetGLq6nRVrygI0OdKu4mZBfxqj7nReGydiZApxpVQWifhJNuqTqRXQEE1KFEPJMSLxVKKGSTVMKhgvdzqbbfOjQZCCLyXGX31ZBL4WtAZDZD>';
 const CAT_IMAGE_URL = 'http://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg';
-const API_AI_TOKEN = '<0ac9a2e05c1649048bc8ea02742bf004>';
+const API_AI_TOKEN = '<fe8a1399f67d45b5bb39d76ab3233d01>';
 const apiAiClient = require('apiai')(API_AI_TOKEN);
 const request = require('request');
 
