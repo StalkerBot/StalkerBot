7da94ae7a15f26c63b8bca4a465d4579902ca067
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-08 21:23:57 +0300

    Nader

--- hello.js
+++ hello.js
@@ -89,11 +89,42 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches a keyword
     // and send back the template example. Otherwise, just echo the text we received.
     switch (messageText) {
-      case 'hey':
-        sendGenericMessage(senderID);
-        break;
-case 'hi':
-sendTextMessage(senderID, "Hiii");
+                case "hello":
+                case "hi":
+                case "ciao":
+                case "hey":
+                case "bonjour":
+                case "good morning":
+                    sendMessage(senderId, "Hello, Who do you want to stalk today? :P");
+                    break;
+		case "what is your name": case "what is your name?": case "what is your name!":
+
+sendMessage(senderId,"My name is StalkerBot, and i am at your service :)");
+break;
+
+	case "what can you do":   case "what do you do": case "what is your job":
+
+sendMessage(senderId,"I can get people's information for you, right now i am working on email extraction :D");
+break;
+
+case "name": case" i am searching for a name": case "the name is":
+sendMessage(senderId,"I will search for the name"); // gets the name from the last message
+// searches for the name
+break;
+
+case "number": case" i am searching for a number": case "the number is":
+sendMessage(senderId,"I will search for the number"); // gets the name from the last message
+// searches for the number
+break;
+
+
+case "email": case" i am searching for an email": case "the email is":
+sendMessage(senderId,"I will search for the email"); // gets the name from the last message
+// searches for the email
+break;
+                default:
+                    sendMessage(senderId,"I don't get it, sorry :(");
+break;
 
       default:
         sendTextMessage(senderID, messageText);
@@ -201,7 +232,7 @@ function callSendAPI(messageData) {
       console.error("Unable to send message.");
       console.error(response);
       console.error(error);
-    }
+    }	
   });  
 }
 
