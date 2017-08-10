b4b6cb0499c29f866af2ccef0f773cbf4894adb2
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-10 11:53:01 +0300

    test

--- hello.js
+++ hello.js
@@ -1,6 +1,4 @@
-//
-// This is main file containing code implementing the Express server and functionality for the Express echo bot.
-//
+// index.js
 'use strict';
 const BootBot = require('bootbot');
 
@@ -9,11 +7,15 @@ const bot = new BootBot({
   verifyToken: 'FB_VERIFY_TOKEN',
   appSecret: 'FB_APP_SECRET'
 });
+
 bot.on('message', (payload, chat) => {
 	const text = payload.message.text;
 	console.log(`The user said: ${text}`);
 });
 
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+	console.log('The user said "hello", "hi", "hey", or "hey there"');
+});
 
 bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
@@ -49,39 +51,3 @@ bot.hear('image', (payload, chat) => {
 		url: 'http://example.com/image.png'
 	});
 });
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const path = require('path');
-var nad=null;
-var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
-
-// The rest of the code implements the routes for our Express server.
-let app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-// Webhook validation
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }
-});
-
-// Display the web page
-app.get('/', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(messengerButton);
-  res.end();
-});
-bot.start();
