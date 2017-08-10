72a98ce691a55719d3b6d74da19b32131c64e0ad
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-10 12:06:46 +0300

    test

--- hello.js
+++ hello.js
@@ -1,56 +1,275 @@
-// index.js
+//
+// This is main file containing code implementing the Express server and functionality for the Express echo bot.
+//
 'use strict';
-const BootBot = require('bootbot');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+var nad=null;
+var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
 
-const bot = new BootBot({
-  accessToken: 'EAATww7C4qdMBAGS5lIQBpQVURCfNlGJoxsIYgjFz2Uwj6k03uFbYdvhO737ZCvdY1ZBBetGLq6nRVrygI0OdKu4mZBfxqj7nReGydiZApxpVQWifhJNuqTqRXQEE1KFEPJMSLxVKKGSTVMKhgvdzqbbfOjQZCCLyXGX31ZBL4WtAZDZD',
-  verifyToken: 'nadstories',
-  appSecret: 'b8c697be93df09e0debdbf09a27109cc'
+// The rest of the code implements the routes for our Express server.
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Webhook validation
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }
+});
+
+// Display the web page
+app.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(messengerButton);
+  res.end();
+});
+
+// Message processing
+app.post('/webhook', function (req, res) {
+  console.log(req.body);
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else if (event.postback) {
+          receivedPostback(event);   
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+// Incoming events handling
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+messageText = message.text.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().trim();
+
+    switch (messageText) {
+                case "hello":
+                case "hi":
+                case "ciao":
+                case "hey":
+                case "bonjour":
+                case "good morning":
+                    sendTextMessage(senderID,"Hello , Who do you want to stalk today? :P");
+                    break;
+
+		case "what is your name": case "what is your name?": case "what is your name!":
+
+sendTextMessage(senderID,"My name is StalkerBot, and i am at your service :)");
+break;
+
+	case "what can you do":   case "what do you do": case "what is your job":
+
+sendTextMessage(senderID,"I can get people's information for you, right now i am working on email extraction :D");
+break;
+
+case "name": case" i am searching for a name": case "the name is":
+sendTextMessage(senderID,"Tell me the name you are searching for"); // gets the name from the last message
+// searches for the name
+break;
+
+case "number": case" i am searching for a number": case "the number is":
+sendTextMessage(senderID,"I will search for the number"); // gets the name from the last message
+// searches for the number
+break;
+
+
+case "email": case" i am searching for an email": case "the email is":
+sendTextMessage(senderID,"I will search for the email"); // gets the name from the last message
+// searches for the email
+break;
+                default:
+                //sendTextMessage(senderID,person(messageText));
+                //sendTextMessage(senderID,numbers (messageText));
+                sendTextMessage(senderID,String(emails(String(messageText))));
+break;
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Hello! This is StalkerBot, write away any name, email address or phone number you are searching for or chat a little bit with me :)");
+}
+
+//////////////////////////
+// Sending helpers
+//////////////////////////
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }	
+  });  
+}
+
+// Set Express to listen out for HTTP requests
+var server = app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port %s", server.address().port);
 });
 
 
-bot.on('message', (payload, chat) => {
-	const text = payload.message.text;
-	console.log(`The user said: ${text}`);
-});
+function emails (txt)
+{
+	var exp = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gim;
+	
+	if(txt.match(exp) !== null)
+		{
+			return txt.match(exp)[0];
+		}
+else return "";
+	
+}
 
-bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
-	console.log('The user said "hello", "hi", "hey", or "hey there"');
-});
+function numbers (txt)
+{
+		var exp = /([0-9-]+[0-9-]+[0-9]+)/g;
+		if(txt.match(exp).length > 0)
+		{	
+			return txt.match(exp)[0];
+		}
+	 else return "";
+}
 
-bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
-	// Send a text message followed by another text message that contains a typing indicator
-	chat.say('Hello, human friend!').then(() => {
-		chat.say('How are you today?', { typing: true });
-	});
-});
 
-bot.hear(['food', 'hungry'], (payload, chat) => {
-	// Send a text message with quick replies
-	chat.say({
-		text: 'What do you want to eat today?',
-		quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
-	});
-});
-
-bot.hear(['help'], (payload, chat) => {
-	// Send a text message with buttons
-	chat.say({
-		text: 'What do you need help with?',
-		buttons: [
-			{ type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
-			{ type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
-			{ type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
-		]
-	});
-});
-
-bot.hear('image', (payload, chat) => {
-	// Send an attachment
-	chat.say({
-		attachment: 'image',
-		url: 'http://example.com/image.png'
-	});
-});
-
-bot.start();
+function person(txt)
+{
+	var exp = /(?:find | find: | find me | stalk | stalk: | search | search for | search for me | find for me | tell me about | who is | who's | whos)(\w+)/;
+	var r = txt.match(exp);
+	///p (.*?) /gi
+	return r[1];
+}
