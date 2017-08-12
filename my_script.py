d813b65cef89e9600e3c0fc3a9ac13dea44d71a3
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 20:34:34 +0300

    A

--- my_script.py
+++ my_script.py
@@ -1,17 +1,27 @@
 import sys, json
+from piplapis.search import SearchAPIRequest
 
-
+#Read data from stdin
+def read_in():
     lines = sys.stdin.readlines()
     # Since our input would only be having one line, parse our JSON data from that
-    json.loads(lines[0])
+    return json.loads(lines[0])
+
+def main():
+    #get our data as an array from read_in()
+    lines = read_in()
+
+    # Sum  of all the items in the providen array
+    total_sum_inArray = 0
+    for item in lines:
+        total_sum_inArray += item
 
 
 
 
 
 
-from piplapis.search import SearchAPIRequest
-request = SearchAPIRequest(email=lines[0], api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
+request = SearchAPIRequest(email=total_sum_inArray, api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
 response = request.send()
 
 #print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
