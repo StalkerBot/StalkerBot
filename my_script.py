cdee6544708cd8d3f63d01458576c2058d6e2ae3
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 20:30:48 +0300

    A

--- my_script.py
+++ my_script.py
@@ -1,21 +1,17 @@
 import sys, json
 
-#Read data from stdin
-def read_in():
+
     lines = sys.stdin.readlines()
     # Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
+    json.loads(lines[0])
 
-def main():
-    #get our data as an array from read_in()
-    lines = read_in()
 
 
 
 
 
 from piplapis.search import SearchAPIRequest
-request = SearchAPIRequest(email=lines, api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
+request = SearchAPIRequest(email=lines[0], api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
 response = request.send()
 
 #print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
@@ -27,6 +23,8 @@ print(response.username)
 #print(", ".join(map(str, response.person.jobs)))
 
 
+
+#print(", ".join(map(str, response.person.relationships)))
+
 if __name__ == '__main__':
     main()
-#print(", ".join(map(str, response.person.relationships)))
