793eb15c32c544fac9bb42017fa65087780cf7e1
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 14:36:14 +0300

    A

--- my_script.py
+++ my_script.py
@@ -1,11 +1,11 @@
 from piplapis.search import SearchAPIRequest
-request = SearchAPIRequest(email=u'nadershakhshir@gmail.com', api_key='SOCIAL-DEMO-gbjr7vchh5hqknincfjhs4dr')
+request = SearchAPIRequest(email=u'nadershakhshir@gmail.com', api_key='SOCIAL-DEMO-cgvn1glx9p2it3copo6txt5b')
 response = request.send()
 
 print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
 print(response.name)
-#print(response.education)
-#print(response.username)
-#print(response.address)
-#print(", ".join(map(str, response.person.jobs)))
-#print(", ".join(map(str, response.person.relationships)))
+print(response.education)
+print(response.username)
+print(response.address)
+print(", ".join(map(str, response.person.jobs)))
+print(", ".join(map(str, response.person.relationships)))
