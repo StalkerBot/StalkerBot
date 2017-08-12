566add772b7311fe52171b5ef8da5bf0bc70c7b3
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 20:55:42 +0300

    A

--- my_script.py
+++ my_script.py
@@ -1,22 +1,31 @@
-import sys, json
-from piplapis.search import SearchAPIRequest
+
+
+
+import sys, json, numpy as np
 
 #Read data from stdin
 def read_in():
     lines = sys.stdin.readlines()
-    # Since our input would only be having one line, parse our JSON data from that
+    #Since our input would only be having one line, parse our JSON data from that
     return json.loads(lines[0])
 
 def main():
     #get our data as an array from read_in()
     lines = read_in()
 
-    # Sum  of all the items in the providen array
-    total_sum_inArray = 0
-    for item in lines:
-        total_sum_inArray += item
-print "A"
-request = SearchAPIRequest(email=u'nadershakhshir@gmail.com', api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
+    #create a numpy array
+    np_lines = np.array(lines)
+
+    #use numpys sum method to find sum of all elements in the array
+    lines_sum = np.sum(np_lines)
+
+    #return the sum to the output stream
+   
+
+
+from piplapis.search import SearchAPIRequest
+
+request = SearchAPIRequest(email=lines_sum, api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
 response = request.send()
 
 #print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
@@ -31,5 +40,7 @@ print(response.username)
 
 #print(", ".join(map(str, response.person.relationships)))
 
+
+#start process
 if __name__ == '__main__':
     main()
