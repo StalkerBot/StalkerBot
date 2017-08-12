06163178043fba1dcb94aafa2cc3ab6b7d660bd8
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 23:41:36 +0300

    A

--- hello.js
+++ hello.js
@@ -8,6 +8,7 @@ const request = require('request');
 const path = require('path');
 var nad=null;
 var unirest = require('unirest');
+var requestify = require('requestify'); 
 var messengerButton = "<html><head><title>StalkerBot</title></head><body><h1>StalkerBot</h1>This is a messenger bot currently in testing phase. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
 //var PythonShell = require('python-shell');
 
@@ -15,6 +16,15 @@ var messengerButton = "<html><head><title>StalkerBot</title></head><body><h1>Sta
   //if (err) throw err;
   //console.log('finished');
 //});
+
+
+var http = require('http');
+
+var options = {
+  host: 'api.pipl.com',
+  port: 80,
+  path: '/search/?email=nadershakhshir@gmail.com&key=SOCIAL-DEMO-5qrzjq10n3vsfeui0g4ymi1c'
+};
 
 
 // The rest of the code implements the routes for our Express server.
@@ -106,16 +116,31 @@ if (messageText.match(exp) !== null)
 sendTextMessage(senderID, "Sending my birds across the globe to bring you this email owner ;)");
 var emaill=messageText.match(exp)[0];
 
-	
-    request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred 
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-  console.log('body:', body); // Print the HTML for the Google homepage. 
 
-sendTextMessage(senderID,body);
-sendTextMessage(senderID,String(body));
-sendTextMessage(senderID,"DONE");
+
+requestify.get('http://api.pipl.com/search/?email=nadragh@yahoo.com&key=SOCIAL-DEMO-5qrzjq10n3vsfeui0g4ymi1c').then(function(response) {
+	// Get the response body
+	response.getBody();
+sendTextMessage(senderID,response.body);
+
+sendTextMessage(senderID,response.body.toString());
 });
+
+
+
+http.get(options, function(resp){
+  resp.on('data', function(chunk){
+    //do something with chunk
+sendTextMessage(senderID,chunk);
+sendTextMessage(senderID,String(chunk));
+sendTextMessage(senderID,"DONE");
+  });
+}).on("error", function(e){
+  console.log("Got error: " + e.message);
+});
+
+
+
         
          
     
