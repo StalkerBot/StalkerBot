0f1f816507181b79a54e5b5e1ce6c060f12224a1
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 15:18:53 +0300

    A

--- hello.js
+++ hello.js
@@ -213,13 +213,13 @@ var exp = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gim;
 if (messageText.match(exp) !== null)
 {
 sendTextMessage(senderID, "Sending my birds across the globe to bring you this email owner ;)");
-var emaill=messageText.match(exp)[0];
+//var emaill=messageText.match(exp)[0];
 
-   var pyshell = new PythonShell('my_script.py');
-   pyshell.on('message', function (message) {
-  sendTextMessage(senderID, message);
-  console.log(message);
-});
+   //var pyshell = new PythonShell('my_script.py');
+   //pyshell.on('message', function (message) {
+  //sendTextMessage(senderID, message);
+  //console.log(message);
+//});
 }
 
 break;
