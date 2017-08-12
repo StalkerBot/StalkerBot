88a56407b42e4dc5594cd9aa0afd330b59808fa7
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 19:58:35 +0300

    A

--- my_script.py
+++ my_script.py
@@ -1,5 +1,5 @@
 from piplapis.search import SearchAPIRequest
-request = SearchAPIRequest(name=u'nader shakhshir', api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
+request = SearchAPIRequest(Name=u'nader shakhshir', api_key='CONTACT-DEMO-fg8iwe5slw9zc2eqb4xd8gu7')
 response = request.send()
 
 #print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
