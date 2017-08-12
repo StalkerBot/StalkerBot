476c943306eceb073768d9b5c5d223fdbdd899be
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 19:28:27 +0300

    A

--- my_script.py
+++ my_script.py
@@ -2,7 +2,7 @@ from piplapis.search import SearchAPIRequest
 request = SearchAPIRequest(email=u'ashrafperformance@gmail.com', api_key='SOCIAL-PREMIUM-DEMO-lug6842a74vedg68olpi51wu')
 response = request.send()
 
-print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
+#print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
 print(response.name)
 print(response.education)
 print(response.username)
