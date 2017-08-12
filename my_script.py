447f5a377313d660ce8f152af7d1398d6f6e5241
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 20:22:58 +0300

    A

--- my_script.py
+++ my_script.py
@@ -4,7 +4,7 @@ import sys, json
 
     lines = sys.stdin.readlines()
     # Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
+    json.loads(lines[0])
 
 
 
