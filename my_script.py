9fcc144ec47fb9353b83d4b5ea1a8c6667a92cb2
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-12 20:16:50 +0300

    A

--- my_script.py
+++ my_script.py
@@ -14,7 +14,7 @@ response = request.send()
 
 #print(response.image.get_thumbnail_url(200, 100, zoom_face=False, favicon=False))
 print(response.name)
-print(response.education)
+#print(response.education)
 print(response.username)
 #print(response.address)
 #print(", ".join(map(str, response.person.jobs)))
