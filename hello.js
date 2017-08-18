277186ae69973c2172d263bc6525185b8a265c53
Nader Shakhshir <nadershakhshir@gmail.com>
2017-08-18 14:05:03 +0300

    Final

--- hello.js
+++ hello.js
@@ -322,9 +322,7 @@ var Z = X.slice(X.indexOf(Y) + Y.length);
                   sendTextMessage(senderID,"it's not time to "+Z+" come on, let's stalk");
 
 }
-
 else if ((messageText.indexOf('eat')>=0 || messageText.indexOf('hungry')>=0 || messageText.indexOf('food')>=0) && ((messageText.indexOf('i want')>=0) || (messageText.indexOf('i am')>=0)))
-
 {
                   sendTextMessage(senderID,"do you think i will order a pizza for you :P?");
 }
